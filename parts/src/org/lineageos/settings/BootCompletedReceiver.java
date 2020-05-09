3c3132a3eaac8f01a5e094c62489f0f82170343e
TheScarastic <warabhishek@gmail.com>
2020-05-09 08:23:43 +0000

    phoenix: Add dynamic thermal profile implementation
    Also add in FileUtils
    
    Change-Id: I4667b24a8a29a418796dc51b245ede0aca51eea6
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -44,11 +44,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             // Ignore
         }
 
-        // Doze
         DiracUtils.initialize(context);
-        DozeUtils.checkDozeService(context);
-        // Force apply our default value for doze if it is not set.
-        DozeUtils.enableDoze(context, DozeUtils.isDozeEnabled(context));
+        ThermalUtils.startService(context);
     }
-
 }
